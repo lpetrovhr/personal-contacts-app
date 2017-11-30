534bdafad8d9de51fd32a2274f9f18441488625c
lpetrovhr <lpetrovhr@gmail.com>
2017-11-30 09:17:29 +0100

    get rid of redux extension that's causing a troubles for windows env

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ const reducer = persistCombineReducers(config, reducers);
 
 function configureStore () {
   
-  let store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+  let store = createStore(reducer, compose(applyMiddleware(thunk)));
 
   let persistor = persistStore(store);
 
